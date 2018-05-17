49afe8bb37cbdd1a8376a7a4f5ffd1bfcb8f4f82
knsonera <kn.sonera@gmail.com>
2018-05-17 13:06:38 -0700

    fixed geodata path

--- static/js/index.js
+++ static/js/index.js
@@ -5,6 +5,5 @@ function initMap() {
     center: {lat: 37.45, lng: -122.16}
   });
 
-  // NOTE: This uses cross-domain XHR, and may not work on older browsers.
-  map.data.loadGeoJson('../static/js/geodata.json');
+  map.data.loadGeoJson('geodata.json');
 }
