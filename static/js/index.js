de0092b12f2c52e698ee3ebea8b1e367f4d65d9a
knsonera <kn.sonera@gmail.com>
2018-05-17 12:53:35 -0700

    changed geo api to google maps

--- static/js/index.js
+++ static/js/index.js
@@ -1,33 +1,10 @@
 var map;
-function init() {
-    map = myMap = new ymaps.Map("map", {
-        center: [59.94, 30.30],
-        zoom: 10,
-        behaviors: ['default', 'scrollZoom'],
-        controls: ['rulerControl']
-    }, {
-            autoFitToViewport: 'always'
-        });
-    var collection = new ymaps.GeoObjectCollection();
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: {lat: 37.45, lng: -122.16}
+  });
 
-    ymaps.geoXml.load('http://maps.yandex.ru/export/usermaps/TRvYO8yGoCpUP5-CEc8jZIX_oIVwR01R/').then(function (res) {
-        map.geoObjects.add(res.geoObjects);
-        map.setBounds(map.geoObjects.getBounds());
-    });
-
-    var searchControl = new ymaps.control.SearchControl();
-    var geoControl = new ymaps.control.GeolocationControl();
-    var zoomControl = new ymaps.control.ZoomControl();
-    var trafficControl = new ymaps.control.TrafficControl();
-    var routeControl = new ymaps.control.RouteEditor();
-
-    map.geoObjects.add(collection);
-
-    map.controls
-        .add(searchControl, { floatIndex: 20 })
-        .add(geoControl, { float: 'none', position: { bottom: 30, left: 5 } })
-        .add(routeControl, { float: 'none', position: { bottom: 65, left: 5 } })
-        .add(zoomControl, { size: 'small', float: 'none', position: { bottom: 65, right: 10 } })
-        .add(trafficControl, { size: 'large', float: 'right' })
-
+  // NOTE: This uses cross-domain XHR, and may not work on older browsers.
+  map.data.loadGeoJson('../static/js/geodata.json');
 }
